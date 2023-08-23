c6ab589c59462f340506efd9ca3a688124d3da60
Abdelouafi Er-raqui <abdelouafierraqui@gmail.com>
2023-08-23 16:37:27 +0100

    strcpy

--- s_shell/string.c
+++ s_shell/string.c
@@ -88,27 +88,7 @@ char *_strcat(char *dest, char *src)
 	return (dest);
 }
 
+
 /*###########################33*/
 
 
-/**
- * _strcpy - copies a string
- * @dest: the destination
- * @src: the source
- *
- * Return: pointer to destination
- */
-char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	if (dest == src || src == 0)
-		return (dest);
-	while (src[i])
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = 0;
-	return (dest);
-}
--- s_shell/string1.c
+++ s_shell/string1.c
@@ -57,5 +57,21 @@ void _puts(char *str)
 }
 
 
+/**
+ * _strcpy - copy string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *s = dest;
 
+	while (*src != '\0')
+	{
+		*s++ = *src++;
+	}
+	*s = '\0';
+	return (dest);
+}
 /*############################*/
